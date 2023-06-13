620a88838886a974cff6d253c8d6ae79cf7078a9
justrisx <fellipevantine12@gmail.com>
2023-06-12 21:03:25 -0300

    Aulas de JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log('A velocidade do seu carro e de ' + vel + 'Km/h')
+
+if (vel > 50){ //Condicao simples
+    console.log('Voce ultrapassou o limite de velocidade, MULTADO!')
+}
+
