b5071332207f5b82c2e8a791feafdee682e507ca
justrisx <fellipevantine12@gmail.com>
2023-06-17 17:49:25 -0300

    aula javascript

---
+++ aula13/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+
+do {
+    console.log('Contagem: ' + c)
+    c++
+} while (c <= 50)
+
+
+
+
+
+/*while( c <= 3000) {
+    console.log('Passo ' + c)
+    c++ // c = c + 1
+}*/
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
---
+++ aula13/node_49adb0eca39e3.tmp.js
@@ -0,0 +1,28 @@
+var c = 1
+
+do {
+    console.log('Contagem: ' + c)
+} while (c < 50)
+
+
+
+
+
+/*while( c <= 3000) {
+    console.log('Passo ' + c)
+    c++ // c = c + 1
+}*/
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
