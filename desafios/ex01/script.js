9dcb0d0148e30c5f8dfe76af2cf0eb3c8bfd31dd
justrisx <fellipevantine12@gmail.com>
2023-06-28 20:52:48 -0300

    desafio 1

---
+++ desafios/ex01/script.js
@@ -0,0 +1,9 @@
+function click1() {
+    window.alert('Voce clicou no botao 1!')
+}
+function click2() {
+    window.alert('Voce clicou no botao 2!')
+}
+function click3() {
+    window.alert('Voce clicou no botao 3!')
+}
